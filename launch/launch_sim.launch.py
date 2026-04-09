172dd36ce7abb566c648fad0b759dce3dd9aa2d6
KoenPothof <koenpothof@gmail.com>
2026-04-09 15:47:25 +0200

    map bij inladen

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -24,7 +24,9 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                    launch_arguments={'gz_args': '-r empty.sdf'}.items(),
+                    launch_arguments={'gz_args': '-r ' + os.path.join(
+                    get_package_share_directory('my_bot'), 'worlds', 'obstakels1.sdf')
+                }.items(),
              )
 
     # De spawner node (Aangepast van spawn_entity.py naar create)
